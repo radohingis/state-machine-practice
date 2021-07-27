52ba8768d0e6dbf9d043e64fd0c8c3679645ae9f
rado.hingis <hingis.radoslav46@gmail.com>
2021-07-27 15:39:55 +0200

    dragndrop excercise

--- src/05/index.js
+++ src/05/index.js
@@ -5,35 +5,43 @@ const elBody = document.body;
 
 const machine = createMachine({
   initial: 'idle',
-  // Set the initial context
-  // Clue: {
-  //   x: 0,
-  //   y: 0,
-  //   dx: 0,
-  //   dy: 0,
-  //   px: 0,
-  //   py: 0,
-  // }
-  // context: ...,
+  context: {
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0,
+    px: 0,
+    py: 0,
+  },
   states: {
     idle: {
       on: {
         mousedown: {
-          // Assign the point
-          // ...
           target: 'dragging',
+          actions: assign({
+            px: (context, event) => event.clientX,
+            py: (context, event) => event.clientY
+          })
         },
       },
     },
     dragging: {
       on: {
         mousemove: {
-          // Assign the delta
-          // ...
-          // (no target!)
+          actions: assign({
+            dx: (context, event) => event.clientX - context.px,
+            dy: (context, event) => event.clientY - context.py
+          })
         },
         mouseup: {
-          // Assign the position
+          actions: assign({
+            dx: 0,
+            dy: 0,
+            px: 0,
+            py: 0,
+            x: context => context.x + context.dx,
+            y: context => context.y + context.dy,
+          }),
           target: 'idle',
         },
       },
@@ -45,10 +53,7 @@ const service = interpret(machine);
 
 service.onTransition((state) => {
   if (state.changed) {
-    console.log(state.context);
-
     elBox.dataset.state = state.value;
-
     elBox.style.setProperty('--dx', state.context.dx);
     elBox.style.setProperty('--dy', state.context.dy);
     elBox.style.setProperty('--x', state.context.x);
@@ -58,7 +63,6 @@ service.onTransition((state) => {
 
 service.start();
 
-// Add event listeners for:
-// - mousedown on elBox
-// - mousemove on elBody
-// - mouseup on elBody
+elBox.addEventListener('mousedown', service.send)
+elBox.addEventListener('mouseup', service.send)
+elBox.addEventListener('mousemove', service.send)
